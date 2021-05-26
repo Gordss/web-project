0800caab5717b0ffaf99c8b518e22a9bfcdb5f69
Kirilka Doncheva <kiki.doncheva393@gmail.com>
2021-05-26 21:41:28 +0300

    Fixing styles and adding csv file download option

--- scripts/upload_archive.js
+++ scripts/upload_archive.js
@@ -5,19 +5,38 @@ window.addEventListener('DOMContentLoaded', () => {
 function uploadArchive(event) {
     event.preventDefault();
 
+    let zip =  getUploadedFile();
+    let zipName = zip['name'];
+
     const formData = new FormData();
-    formData.append('file', getUploadedFile());
+    formData.append('file', zip);
 
     fetch('upload.php', {
         method: 'POST',
         body: formData
     }).then(response => response.text())
         .then(text => {
-            console.log(text);
             document.querySelector('textarea').innerText = text;
+            createCsvDownloadLink(text, zipName);
         });
 }
 
 function getUploadedFile() {
     return document.getElementById('file-input').files[0];
+}
+
+function createCsvDownloadLink (csvContent, zipName){
+    let fileName = zipName.substring(0, zipName.length-3).concat("csv");
+
+    console.log(fileName);
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.getElementById("download-csv");
+    link.setAttribute("href", 'data:text/csv;charset=utf-8,' + encodedUri);
+    link.setAttribute("download", fileName);
+
+    document.getElementById("download-link-label").innerText = fileName;
+
+    link.style.display = "inline";
+
 }
