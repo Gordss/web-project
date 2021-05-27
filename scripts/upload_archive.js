7a653c752915ae0484874693c4a8173b44adfbfb
Bobi <bobissss84@gmail.com>
2021-05-27 12:41:28 +0300

    Add options for fields' colours

--- scripts/upload_archive.js
+++ scripts/upload_archive.js
@@ -1,6 +1,18 @@
+var currentTypeIndex = undefined;
+
 window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('form').addEventListener('submit', uploadArchive);
+    document.querySelectorAll('input[type=color]').forEach(input => {
+        input.addEventListener('input', onColorChange);
+    })
 });
+
+
+function onColorChange() {
+    document.querySelectorAll('#csv-result-placeholder span').forEach(span => {
+        span.style.color = getColourForLine(span.innerHTML);
+    });
+}
 
 function uploadArchive(event) {
     event.preventDefault();
@@ -18,12 +30,55 @@ function uploadArchive(event) {
         body: formData
     }).then(response => {
         response.text().then(text => {
-            document.querySelector('textarea').innerHTML = text
+            const resultPlaceholder = document.getElementById('csv-result-placeholder');
+            resultPlaceholder.innerHTML = '';
+            const lines = text.split("\n");
+            lines.pop(); // There is an empty line in the end
+            setTypeIndex(lines[0]);
+            lines.forEach(line => {
+                const lineElement = document.createElement('span');
+                lineElement.style.color = getColourForLine(line);
+                lineElement.innerHTML = line;
+                resultPlaceholder.appendChild(lineElement);
+            })
             if (response.status === 200) {
                 createCsvDownloadLink(text, zip['name']);
             }
         });
     })
+}
+
+function setTypeIndex(headerLine) {
+    const fields = headerLine.split(',');
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] === 'type') {
+            currentTypeIndex = i;
+            return;
+        }
+    }
+    currentTypeIndex = undefined;
+}
+
+function getColourForLine(line) {
+    const defaultColor = document.getElementById('default-color').value;
+    if (!currentTypeIndex) {
+        return defaultColor;
+    }
+    switch (line.split(',')[currentTypeIndex].toLowerCase()) {
+        case 'txt':
+        case 'md':
+        case 'doc':
+        case 'docx':
+            return document.getElementById('txt-files-color').value;
+        case 'jpg':
+        case 'png':
+        case 'gif':
+            return document.getElementById('img-files-color').value;
+        case 'directory':
+            return document.getElementById('dir-files-color').value;
+        default:
+            return defaultColor;
+    }
 }
 
 function getUploadedFile() {
--- upload_page.php
+++ upload_page.php
@@ -33,28 +33,49 @@
                             <h3>Choose a zip file to upload & convert to CSV:</h3>
                         </label>
                         <input id="file-input" type="file" name="file">
-                        <fieldset id="options-fieldset">
-                            <legend>Include fields</legend>
-                            <label>
-                                Name
-                                <input type="checkbox" name="include-name" checked/>
-                            </label>
-                            <label>
-                                Parent Name
-                                <input type="checkbox" name="include-parent-name" checked/>
-                            </label>
-                            <label>
-                                Content Length
-                                <input type="checkbox" name="include-content-length" checked/>
-                            </label>
-                            <label>
-                                Type
-                                <input type="checkbox" name="include-type" checked/>
-                            </label>
-                            <label>MD5 sum
-                                <input type="checkbox" name="include-md5_sum" checked/>
-                            </label>
-                        </fieldset>
+                        <div id="options">
+                            <fieldset id="include-options-fieldset">
+                                <legend>Include fields</legend>
+                                <label>
+                                    Name
+                                    <input type="checkbox" name="include-name" checked/>
+                                </label>
+                                <label>
+                                    Parent Name
+                                    <input type="checkbox" name="include-parent-name" checked/>
+                                </label>
+                                <label>
+                                    Content Length
+                                    <input type="checkbox" name="include-content-length" checked/>
+                                </label>
+                                <label>
+                                    Type
+                                    <input type="checkbox" name="include-type" checked/>
+                                </label>
+                                <label>MD5 sum
+                                    <input type="checkbox" name="include-md5_sum" checked/>
+                                </label>
+                            </fieldset>
+                            <fieldset>
+                                <legend>Visualization options</legend>
+                                <label>
+                                    Text files
+                                    <input id="txt-files-color" type="color"/>
+                                </label>
+                                <label>
+                                    Image files
+                                    <input id="img-files-color" type="color"/>
+                                </label>
+                                <label>
+                                    Directories
+                                    <input id="dir-files-color" type="color"/>
+                                </label>
+                                <label>
+                                    Default
+                                    <input id="default-color" type="color"/>
+                                </label>
+                            </fieldset>
+                        </div>
                         <button id="file-input-btn" type="submit">Convert</button>
                     </form>
                 </section>
@@ -63,8 +84,9 @@
             <td>
                 <h3 id="download-label">Converted CSV file:</h3>
                 <a id="download-csv"><h3 id="download-link-label"></h3></a>
-                <textarea>
-                </textarea>
+                <p id="csv-result-placeholder">
+
+                </p>
             </td>
         </tr>
     </table>
