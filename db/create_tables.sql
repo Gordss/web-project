1e12cda284ce0939b6a365f7d1f61edd89eed786
Bobi <bobissss84@gmail.com>
2021-05-28 12:54:53 +0300

    Rework DB layer

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,27 +1,25 @@
-CREATE DATABASE IF NOT EXISTS web_project;
-
-CREATE TABLE IF NOT EXISTS web_project.users (
+CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(64) NOT NULL UNIQUE,
   password VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS web_project.archives (
+CREATE TABLE IF NOT EXISTS archives (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
-  uploaded_at DATETIME NOT NULL DEFAULT (current_timestamp),
-
-  CONSTRAINT `User ID foreign key` FOREIGN KEY(user_id) REFERENCES web_project.users (id)
+  uploaded_at DATETIME NOT NULL DEFAULT current_timestamp,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS web_project.nodes (
+CREATE TABLE IF NOT EXISTS nodes (
     archive_id      INT NOT NULL,
-    name            VARCHAR(256) NOT NULL,
+    name            VARCHAR(128) NOT NULL,
     parent_name     VARCHAR(256),
     content_length  INT,
     type            VARCHAR(64),
     md5_sum         VARCHAR(32),
-
     PRIMARY KEY (name,archive_id),
-    CONSTRAINT `Parent name self key` FOREIGN KEY (parent_name) REFERENCES web_project.nodes (name)
+    FOREIGN KEY (parent_name)
+        REFERENCES nodes(name)
 );
