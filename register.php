dde66956df166087a87dca36f702cd39c1dc692d
Bobi <bobissss84@gmail.com>
2021-05-30 18:26:38 +0300

    Username&password validation on registration

--- register.php
+++ register.php
@@ -19,22 +19,41 @@
         </form>
         <p>Already registered? <a href="login.php">Log in here</a></p>
         <?php
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            die;
+        }
+
         require_once "Storage.php";
 
         if (!isset($_POST['username']) || !isset($_POST['password']) || empty($_POST['username']) || empty($_POST['password'])) {
-            die;
+            printError('Both username and password have to be set');
         }
 
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        if (strlen($username) < 8) {
+            printError('Username must be at least 8 characters in length');
+        }
+
+        if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $password)) {
+            printError('Password must be at least 8 characters in length and contain at least one number, one upper case letter, one lower case letter and one special character.');
+        }
+
         $error = Storage::getInstance()->registerUser($username, $password);
-        if (empty($error)) {
-            header('Location: login.php');
+        if (!empty($error)) {
+            printError('This username is already taken');
+        }
+        header('Location: login.php');
+
+        function printError($error)
+        {
+            echo '<div class="error">' . $error . '</div>';
             die;
         }
+
         ?>
-        <div class="error">Registration unsuccessful: Username is already taken</div>
     </section>
 </main>
 </body>
