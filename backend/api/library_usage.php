f1d17a2d1ba39ccf83ff200ab09d9d666acbc73b
Niki <nickoleta.ml@gmail.com>
2022-01-26 11:08:03 +0200

    Configuration constant for library usage

--- backend/api/library_usage.php
+++ backend/api/library_usage.php
@@ -1,8 +1,5 @@
 <?php
     require_once "./config.php";
-    require_once './../utils/send_response.php';
-
-    $post = json_decode(file_get_contents("php://input"), true);
 
     $nolibrary = Config::$REMOVE_FORGOT_PASSWORD_FUNCTION;
     header("No-library: $nolibrary");
--- backend/api/login.php
+++ backend/api/login.php
@@ -1,5 +1,4 @@
 <?php
-    require_once "./config.php";
     require_once 'Storage.php';
     require_once './../utils/send_response.php';
 
